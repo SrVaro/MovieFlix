26bee55acdf5506042a56155d4962fa598609c93
Andres Rojas <rojas.jorge730@gmail.com>
2019-09-30 16:17:02 +0200

    LecturaDatos
    Se crea la clase lectura datos, y se crean los metodos LeerInt y LeerString

--- src/interfazGrafica/InterfazGrafica.java
+++ src/interfazGrafica/InterfazGrafica.java
@@ -1,5 +1,12 @@
 package interfazGrafica;
 
+/** 
+ * 
+ * @author Andres
+ * 
+ * version 0.1
+ *
+ */
 public class InterfazGrafica {
 
 }
---
+++ src/utilidades/LecturaDatos.java
@@ -0,0 +1,38 @@
+package utilidades;
+/**
+ * 
+ * Autor Andres
+ * 
+ * Version 0.1
+ */
+
+import java.util.Scanner;
+
+/**
+ * 
+ * Se pide un entero por Scanner
+ */
+public class LecturaDatos {
+
+	public static int LeerInt(){
+		return new Scanner (System.in).nextInt();
+	}
+	
+	public static int LeerInt (String mensaje) {
+		System.out.println(mensaje);
+		return LeerInt();
+	}
+	
+	/**
+	 * 
+	 * Se pide un String por Scanner
+	 */
+	public static String LeerString() {
+		return new Scanner (System.in).nextLine();
+	}
+	
+	public static String LeerString(String mensaje) {
+		System.out.println(mensaje);
+		return LeerString();
+	}
+}
