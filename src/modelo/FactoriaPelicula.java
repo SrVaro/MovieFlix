dc134336615d91012fbc594bf656139a2a6fd482
Andres Rojas <rojas.jorge730@gmail.com>
2019-09-30 17:56:53 +0200

    cambio metodo

--- src/modelo/FactoriaPelicula.java
+++ src/modelo/FactoriaPelicula.java
@@ -20,43 +20,45 @@ public class FactoriaPelicula {
 
 	public static Pelicula pedirPelicula() {
 		System.out.println("Adjuntar nueva película");
-		
+
 		String nombre = LecturaDatos.LeerString("Introduzca el nombre de la pelicula");
 		int annoEstreno = (LecturaDatos.LeerInt("Introduzca el año de estreno de la Película"));
 		CATEGORIA categoria;
 		int opcion = 0;
-		do {
+		while (opcion > 0 && opcion <= 6) {
 			InterfazGrafica.mensajeCategoriaPelicula();
 			opcion = LecturaDatos.LeerInt();
-		
-		switch(opcion) {
-		case 1:
-		categoria = CATEGORIA.POLICIACA;
-		break;
-		 
-		case 2:
-		categoria = CATEGORIA.ROMANTICA;
-		break;
-	
-		case 3:
-		categoria = CATEGORIA.AVENTURAS;
-		break;
-		
-		case 4:
-		categoria = CATEGORIA.COMEDIA;
-		break;
-	
-		case 5:
-		categoria = CATEGORIA.ANIMACION;
-		break;
-		
-		case 6:
-		categoria = CATEGORIA.THRILLER;
-		break;
-	}while (opcion > 6 || opcion < 0)
-		
-		return new Pelicula(nombre, annoEstreno , categoria);
-		
+
+			switch (opcion) {
+			case 1:
+				categoria = CATEGORIA.POLICIACA;
+				break;
+
+			case 2:
+				categoria = CATEGORIA.ROMANTICA;
+				break;
+
+			case 3:
+				categoria = CATEGORIA.AVENTURAS;
+				break;
+
+			case 4:
+				categoria = CATEGORIA.COMEDIA;
+				break;
+
+			case 5:
+				categoria = CATEGORIA.ANIMACION;
+				break;
+
+			case 6:
+				categoria = CATEGORIA.THRILLER;
+				break;
+			}
+			
+
+			return new Pelicula(nombre, annoEstreno, categoria);
+
+		}
 	}
-}
+
 }
