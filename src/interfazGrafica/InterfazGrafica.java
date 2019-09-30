a1952c7a91e260e4f8ddf60ff618512d90adcbbd
mccurado <mc_curado@hotmail.com>
2019-09-30 17:35:41 +0200

    mensajes para dar de alta una pelicula

--- src/interfazGrafica/InterfazGrafica.java
+++ src/interfazGrafica/InterfazGrafica.java
@@ -8,5 +8,30 @@ package interfazGrafica;
  *
  */
 public class InterfazGrafica {
+	
+	public InterfazGrafica() {
+		
+	}
+	/**
+	 * @author M Carmen
+	 * 
+	 * mensajes para dar de alta una pelicula
+	 */
+	public static void mensajeNombrePelicula() {
+		System.out.println("\nIntroduzca el nombre de la película:");
+	}
+	
+	public static void mensajeAnnoPelicula() {
+		System.out.println("\n Introduzca el año del estreno:");
+	}
+	public static void mensajeCategoriaPelicula() {
+		System.out.println("\nIntroduzca a que categoria pertenece:");
+		System.out.println("\tPonga 1 para seleccionar categoria POLICIACA");
+        System.out.println("\tPonga 2 para seleccionar categoria ROMANTICA");
+        System.out.println("\tPonga 3 para seleccionar categoria AVENTURAS");
+        System.out.println("\tPonga 4 para seleccionar categoria COMEDIA");
+        System.out.println("\tPonga 5 para seleccionar categoria ANIMACION");
+        System.out.println("\tPonga 6 para seleccionar categoria THRILLER");
+	}
 
 }
