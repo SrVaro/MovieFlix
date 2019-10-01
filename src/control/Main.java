e81d1dba9c9373413bfce671de103dd995c07b3a
Andres Rojas <rojas.jorge730@gmail.com>
2019-10-01 14:48:58 +0200

    Update Main.java

--- src/control/Main.java
+++ src/control/Main.java
@@ -1,14 +1,7 @@
 package control;
 
-import datos.DatoPelicula;
-import modelo.FactoriaPelicula;
-import modelo.Pelicula;
-
 public class Main {
 	public static void main(String[] args) {
-		//Control.empezarPrograma();
-		
-		DatoPelicula n = new DatoPelicula();
-		n.listarPeliculasMayMen();
+		Control.empezarPrograma();
 	}
 }
