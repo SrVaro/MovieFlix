bfe69fe5f527da743e08f211833c49f33b1da4e4
Andres Rojas <rojas.jorge730@gmail.com>
2019-10-01 15:17:38 +0200

    Update Control.java

--- src/control/Control.java
+++ src/control/Control.java
@@ -65,7 +65,7 @@ public class Control {
 			s.listarPeliculasMayMen();
 			break;
 		case 10:
-			
+			s.listatPeliculasVal();
 		}
 		InterfazGrafica.mostrarMenuPausa();
 		LecturaDatos.LeerString();
