dfc7c6959aa1578dfab46ca99cf5681780e1b9a3
Andres Rojas <rojas.jorge730@gmail.com>
2019-10-02 10:21:48 +0200

    Update FactoriaPelicula.java

--- src/modelo/FactoriaPelicula.java
+++ src/modelo/FactoriaPelicula.java
@@ -2,6 +2,10 @@ package modelo;
 
 import utilidades.LecturaDatos;
 import modelo.Pelicula;
+
+import java.io.IOException;
+import java.util.InputMismatchException;
+
 import interfazGrafica.InterfazGrafica;
 
 /**
@@ -27,39 +31,62 @@ public class FactoriaPelicula {
 		InterfazGrafica.mensajeAnnoPelicula();
 		int annoEstreno = (LecturaDatos.LeerInt());
 
-		CATEGORIA categoria = null;
+		boolean noConseguido = true;
 		InterfazGrafica.mensajeValPelicula();
-		int valoracion = LecturaDatos.LeerInt();
-
-		int opcion = 0;
-
+		int valoracion = 0;
 		do {
-
-			InterfazGrafica.mensajeCategoriaPelicula();
-			opcion = LecturaDatos.LeerInt();
-
-			switch (opcion) {
-			case 1:
-				categoria = CATEGORIA.POLICIACA;
-				break;
-			case 2:
-				categoria = CATEGORIA.ROMANTICA;
-				break;
-			case 3:
-				categoria = CATEGORIA.AVENTURA;
-				break;
-			case 4:
-				categoria = CATEGORIA.COMEDIA;
-				break;
-			case 5:
-				categoria = CATEGORIA.ANIMACION;
-				break;
-			case 6:
-				categoria = CATEGORIA.THRILLER;
-				break;
+			try {
+				valoracion = LecturaDatos.LeerInt();
+				if ((valoracion > 5) || (valoracion < 0)) {
+					throw new IOException("Rango no valido, introduzca otro rango");
+				} else {
+					noConseguido = false;
+				}
+			} catch (IOException e) {
+				System.out.println("Problema: " + e.getMessage());
 			}
 
-		} while (opcion < 1 && opcion > 6);
+		} while (noConseguido);
+		
+		noConseguido = true;
+		
+		CATEGORIA categoria = null;
+		InterfazGrafica.mensajeCategoriaPelicula();
+
+		int opcion = 0;
+		do {
+			try {
+				opcion = LecturaDatos.LeerInt();
+				if ((opcion > 6) || (opcion < 1)) {
+					throw new IOException("Rango no valido, introduzca otra opcion");
+				} else {
+					noConseguido = false;
+					switch (opcion) {
+					case 1:
+						categoria = CATEGORIA.POLICIACA;
+						break;
+					case 2:
+						categoria = CATEGORIA.ROMANTICA;
+						break;
+					case 3:
+						categoria = CATEGORIA.AVENTURA;
+						break;
+					case 4:
+						categoria = CATEGORIA.COMEDIA;
+						break;
+					case 5:
+						categoria = CATEGORIA.ANIMACION;
+						break;
+					case 6:
+						categoria = CATEGORIA.THRILLER;
+						break;
+					}
+				}
+			} catch (IOException e) {
+				System.out.println("Problema: " + e.getMessage());
+			}
+
+		} while (noConseguido);
 
 		return new Pelicula(nombre, annoEstreno, categoria, valoracion);
 	}
